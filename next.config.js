a284c3c918e9d7640643b9f3596514ff50cdc2dc
abood123-dev <152815238+abood123-dev@users.noreply.github.com>
2024-09-13 07:23:42 -0700

    it

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 // next.config.js
 module.exports = {
+  cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: "[name]__[local]___[hash:base64:5]",
+  }, 
     assetPrefix: process.env.NODE_ENV === 'production' ? '/<Real-estate-project>/' : '',
     images: {
       domains: ['th.bing.com'], // Add your allowed domains here
